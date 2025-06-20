d76b7e514c00c6b60d52be29ac0c86ed69b26bac
terrorsl <terror85@gmail.com>
2025-06-20 15:26:50 +0300

    append crtdbg

--- ogre_studio/src/main.cpp
+++ ogre_studio/src/main.cpp
@@ -3,7 +3,9 @@
 
 int main(int argc, char **argv)
 {
+#if defined(WIN32) && defined(_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
 	QApplication app(argc, argv);
 	QMainWindow* window = new MainWindow();
