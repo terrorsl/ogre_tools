39fa7fff04d8e0b7b23b9bb1afa41be4a860c288
terrorsl <terror85@gmail.com>
2025-06-27 17:55:14 +0300

    append lib

---
+++ library/bullet3-3.25/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: E:/work/ogre_tools/library/bullet3-3.25
+# Build directory: E:/work/ogre_tools/library/bullet3-3.25/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Extras")
+subdirs("src")
+subdirs("test")
